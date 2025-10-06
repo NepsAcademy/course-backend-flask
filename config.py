3462141eaf97e211a18e855206e2aa484cbdc47e
Tiago Barros Pires <tiagobarrospires@gmail.com>
2025-10-06 15:16:38 -0300

    fix(config): add default value

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "mysecretkey")
     APP_TITLE = "Flask REST API Course"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "mysecretkey")
     JWT_TOKEN_LOCATION = ["headers"]
 
     @staticmethod
